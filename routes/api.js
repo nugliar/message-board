052781b8b5649017a428b9a9d40e82c8cf1ab979
nugliar <hashan.hall@gmail.com>
2021-11-16 17:32:16 -0600

    init

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
 'use strict';
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
+
+  app.route('/api/threads/:board')
+    .post((req, res) => {
+      console.log(req.body);
+    });
+
   app.route('/api/replies/:board');
 
 };
